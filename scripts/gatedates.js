384ad829c92d5074c7ea179991a16b1b309035a7
marydingiswayo <mdingiswayo@byupathway.edu>
2025-10-27 15:57:35 +0200

    ff

---
+++ scripts/gatedates.js
@@ -0,0 +1,5 @@
+// Dynamically insert the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Dynamically insert the last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
